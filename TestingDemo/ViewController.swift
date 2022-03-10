7eda812e9e8f5354d2f6f2364bfe405ab8849ec7
islom-babaev-iTechArt-Group <islom.babaev@slicelife.com>
2022-03-10 15:06:03 +0500

    Initial Commit

---
+++ TestingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingDemo
+//
+//  Created by Islom Babaev on 10/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
